1fe4266d4afe0841a6723ce8d48d7ea129e78a7b
Cecilia <waterfull.app@gmail.com>
2024-02-08 13:19:59 +0100

    staging

--- src/pages/find.vue
+++ src/pages/find.vue
@@ -1,7 +1,7 @@
 <script>
   export default {
   }
-</script>
+</script> 
 <template>
   <div class="flex items-center justify-center flex-wrap bg-teal p-6">
     <div class="flex flex-wrap justify-center">
