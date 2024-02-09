cf08be2e50871d0106380453c51d01ae7fe34762
Cecilia <waterfull.app@gmail.com>
2024-02-09 14:24:39 +0100

    fine tuning

--- src/components/StepE.vue
+++ src/components/StepE.vue
@@ -3,18 +3,23 @@
     <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
       {{ $t('addSteps.step5.text1') }}
     </h1>
-    <div class="flex justify-center mx-auto flex-col px-1 py-8">
+    <div class="flex justify-center mx-auto flex-col px-1 py-0">
       <input class="inputfile"
       type="file" name="file" id="file"
       @change="handleImages($event)"
       accept="image/*"
       />
-      <label class="flex justify-center cursor-pointer" for="file"><img class='max-w-16 cursor-pointer' src="../assets/upload.jpg"/></label>
-      <div class="flex justify-center">
-        <img class='max-w-32' :src="display"/>
+      <label class="flex justify-center cursor-pointer h-24" for="file">
+        
+        <svg data-v-44324f62="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="icon"><title data-v-44324f62="">Upload Image</title><g data-v-44324f62="" id="Upload_Image" data-name="Upload Image"><g data-v-44324f62="" id="_Group_" data-name="<Group>"><g data-v-44324f62="" id="_Group_2" data-name="<Group>"><g data-v-44324f62="" id="_Group_3" data-name="<Group>"><circle data-v-44324f62="" id="_Path_" data-name="<Path>" cx="18.5" cy="16.5" r="5" style="fill: none; stroke: rgb(255, 255, 255); stroke-linecap: round; stroke-linejoin: round;"></circle></g><polyline data-v-44324f62="" id="_Path_2" data-name="<Path>" points="16.5 15.5 18.5 13.5 20.5 15.5" style="fill: none; stroke: rgb(255, 255, 255); stroke-linecap: round; stroke-linejoin: round;"></polyline><line data-v-44324f62="" id="_Path_3" data-name="<Path>" x1="18.5" y1="13.5" x2="18.5" y2="19.5" style="fill: none; stroke: rgb(255, 255, 255); stroke-linecap: round; stroke-linejoin: round;"></line></g><g data-v-44324f62="" id="_Group_4" data-name="<Group>"><polyline data-v-44324f62="" id="_Path_4" data-name="<Path>" points="0.6 15.42 6 10.02 8.98 13" style="fill: none; stroke: rgb(255, 255, 255); stroke-linecap: round; stroke-linejoin: round;"></polyline><polyline data-v-44324f62="" id="_Path_5" data-name="<Path>" points="17.16 11.68 12.5 7.02 7.77 11.79" style="fill: none; stroke: rgb(255, 255, 255); stroke-linecap: round; stroke-linejoin: round;"></polyline><circle data-v-44324f62="" id="_Path_6" data-name="<Path>" cx="8" cy="6.02" r="1.5" style="fill: none; stroke: rgb(255, 255, 255); stroke-linecap: round; stroke-linejoin: round;"></circle><path data-v-44324f62="" id="_Path_7" data-name="<Path>" d="M19.5,11.6V4A1.5,1.5,0,0,0,18,2.5H2A1.5,1.5,0,0,0,.5,4V15A1.5,1.5,0,0,0,2,16.5H13.5" style="fill: none; stroke: rgb(255, 255, 255); stroke-linecap: round; stroke-linejoin: round;"></path></g></g></g></svg>
+
+
+      </label>
+      <div :class="show ? '' : 'inputfile'" class="flex justify-center">
+        <img class='w-32 h-32 object-scale-down' :src="display"/>
       </div>
     </div>
-    <div :class="hiddenTrig" class="flex justify-center mx-auto flex px-5 py-8">
+    <div :class="hiddenTrig" class="flex justify-center mx-auto flex px-5 py-0">
       <a @click.prevent="goToStep(4)" ref="previous" href="/"
         ><img class="steparr w-16" src="../assets/previous.svg" alt="previous"
       /></a>
@@ -78,6 +83,7 @@ img {
 export default {
   data() {
     return {
+      show: false,
       choice: '',
       activeTab: 0,
       images: '',
@@ -98,6 +104,7 @@ export default {
         this.display = e.target.result
       }
       reader.readAsDataURL(files.target.files[0])
+      this.show = true
     },
     async imgur() {
       this.hiddenTrig = 'hidden'
