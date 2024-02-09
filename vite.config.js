a12ad6b14375af972836364fe98db539980caef0
Cecilia <waterfull.app@gmail.com>
2024-02-09 20:41:50 +0100

    site map

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { resolve, dirname } from 'node:path'
 import { fileURLToPath } from 'url'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 import dns from 'dns'
+import Sitemap from 'vite-plugin-sitemap'
 dns.setDefaultResultOrder('verbatim')
 
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
     VueI18nPlugin({
       include: resolve(dirname(fileURLToPath(import.meta.url)), './src/locales/**'),
     }),
+    Sitemap(),
   ],
   server: {
     host: 'localhost',
