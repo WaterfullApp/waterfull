73438ba86e67da109127cd485be3553c2b1dea57
BelaProDev <160723695+BelaProDev@users.noreply.github.com>
2024-12-24 00:06:26 +0000

    footer

--- src/App.vue
+++ src/App.vue
@@ -106,6 +106,6 @@ export default {
       </nav>
     </header>
     <router-view></router-view>
-    <footer class="flex justify-end"><a href="mailto:info@waterfull.org">info@waterfull.org</a></footer>
+    <footer class="flex justify-end p-4 m-3"><a class="text-white font-mono" href="mailto:info@waterfull.org">info@waterfull.org</a></footer>
   </div>
 </template>
